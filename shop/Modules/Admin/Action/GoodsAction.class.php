ecfa31ebe436479f9e115ce6faefcdf6b0c13764
MrC <1027566763@qq.com>
2013-12-05 13:28:17 +0800

    cc

--- shop/Modules/Admin/Action/GoodsAction.class.php
+++ shop/Modules/Admin/Action/GoodsAction.class.php
@@ -2,6 +2,11 @@
 
 Class GoodsAction extends Action{
 	public function index(){
+		$goods=M('goods');
+		$result=$goods->select();
+		 
+      $this->assign('list',$result);
+		
 	    $this->display();
 	}
 
