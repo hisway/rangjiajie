a02eb8e517d6f09663677e29a9debc5b5408028e
MrC <1027566763@qq.com>
2013-12-13 16:11:01 +0800

    cc

--- shop/Modules/Admin/Action/GoodsAction.class.php
+++ shop/Modules/Admin/Action/GoodsAction.class.php
@@ -2,10 +2,20 @@
 
 Class GoodsAction extends Action{
 	public function index(){
-		
+		import('ORG.Util.Page');
 		$goods=M('Goods');
-		$result=$goods->select();
-      $this->assign('list',$result);
+			$count      = $goods->count();// 查询满足要求的总记录数
+
+    $Page       = new Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数
+
+    $show       = $Page->show();// 分页显示输出
+
+// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+
+    $list = $goods->limit($Page->firstRow.','.$Page->listRows)->select();
+      $this->assign('page',$show);// 赋值分页输出
+
+      $this->assign('list',$list);
 	    $this->display();
 	}
 	
@@ -20,8 +30,9 @@ Class GoodsAction extends Action{
 	
 	public function add(){
 		
+$goods=D('Goods');
 
-		
+	
 		
 	}
 	
